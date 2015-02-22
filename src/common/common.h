311a9d1f0eb9b4a2650d99ee15041bd761f5f1db
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-02-22 20:26:21 +0700

    Исправлена ошибка компиляции в MSVC 2012+ без _CRT_SECURE_NO_WARNINGS

--- src/common/common.h
+++ src/common/common.h
@@ -1,7 +1,7 @@
 #if !defined COMMON_H_INCLUDED
 #define COMMON_H_INCLUDED
 
-#if defined_MSC_VER && !defined _CRT_SECURE_NO_WARNINGS
+#if defined _MSC_VER && !defined _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
