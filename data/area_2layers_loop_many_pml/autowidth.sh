8961781ff54ea7b56794bf10b9bacb79f7202c3c
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-04-20 22:25:12 +0700

    Обновление 1 теста на толщину PML слоя

--- data/area_2layers_loop_many_pml/autowidth.sh
+++ data/area_2layers_loop_many_pml/autowidth.sh
@@ -2,6 +2,7 @@
 
 for i in `LANG=C seq 50 10 200`
 do
+	echo "==============================================================================="
 	cd "data/area_2layers_loop_many_pml"
 	position=$((${i}+600))
 	cat "autogen_template.geo" | sed "s/REPLACEME/${position}/g" > "autogen.geo"
@@ -9,6 +10,8 @@ do
 	gmsh -3 -optimize -optimize_netgen "autogen.geo"
 	cd ../..
 	echo -e "${i}\n" > "pml_width.txt"
+	echo "==============================================================================="
+	echo "Testing with width = ${i}"
 	./vfem_classic | tee "classic_${i}.txt"
 	./vfem_pml | tee "pml_${i}.txt"
 done
