c4b31b55646fb1b79576eefcd974e66fd37a4ecb
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-05-07 02:51:46 +0700

    Добавил костыль, чтобы gmsh строил нормально две сетки сразу

--- data/area_2layers_loop_universal_pml/universal_test.sh
+++ data/area_2layers_loop_universal_pml/universal_test.sh
@@ -21,17 +21,17 @@ rm -f "${OUT_FILE_1}" "${OUT_FILE_2}" 2>/dev/null
 
 cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
 sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 1;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
-sed "s/SECOND_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 1;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
+sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 1;\nMesh.OptimizeNetgen = 1;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
 "${GMSH}" - "${OUT_FILE}"
 if [[ $? -ne 0 ]] ; then
 	cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
 	sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
-	sed "s/SECOND_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
+	sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
 	"${GMSH}" - "${OUT_FILE}"
 	if [[ $? -ne 0 ]] ; then
 		cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
 		sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
-		sed "s/SECOND_PHASE_HERE/Mesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
+		sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
 		"${GMSH}" - "${OUT_FILE}"
 	fi
 fi
