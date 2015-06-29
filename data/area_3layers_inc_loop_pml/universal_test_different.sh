ecf0b0e63988a5979a101fcd30bc6f513a15f3e3
Peter Zhigalov <peter.zhigalov@gmail.com>
2015-06-30 05:53:32 +0700

    Дополнил скрипты

--- data/area_3layers_inc_loop_pml/universal_test_different.sh
+++ data/area_3layers_inc_loop_pml/universal_test_different.sh
@@ -22,19 +22,21 @@ cd "${CURR_DIR}/${RESULT_DIR}/${MESH_DIR}"
 cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
 sed "s/FIRST_PHASE_HERE//g ; s/SECOND_PHASE_HERE//g" > "${OUT_FILE}"
 ${GMSH} -3 -optimize -optimize_netgen -o "${OUT_FILE_2}" "${OUT_FILE}"
-"${CURR_DIR}/crop_mesh" "${OUT_FILE_2}" "${OUT_FILE_1}" 31 32 33 52
 if [[ $? -ne 0 ]] ; then
-	cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
-	sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
-	sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
-	${GMSH} - "${OUT_FILE}"
+	#cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
+	#sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
+	#sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 1;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
+	#${GMSH} - "${OUT_FILE}"
+	${GMSH} -3 -optimize -o "${OUT_FILE_2}" "${OUT_FILE}"
 	if [[ $? -ne 0 ]] ; then
-		cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
-		sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
-		sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
-		${GMSH} - "${OUT_FILE}"
+		#cat "${INP_FILE}" | sed "s/PML_BEGIN/${PML_BEGIN}/g ; s/PML_WIDTH/${PML_WIDTH}/g" |
+		#sed "s/FIRST_PHASE_HERE/Mesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_1}\";\n/g" |
+		#sed "s/SECOND_PHASE_HERE/Mesh 1;\nMesh.Optimize = 0;\nMesh.OptimizeNetgen = 0;\nMesh 3;\nSave \"${OUT_FILE_2}\";\n/g" > "${OUT_FILE}"
+		#${GMSH} - "${OUT_FILE}"
+		${GMSH} -3 -o "${OUT_FILE_2}" "${OUT_FILE}"
 	fi
 fi
+"${CURR_DIR}/crop_mesh" "${OUT_FILE_2}" "${OUT_FILE_1}" 31 32 33 52
 
 cd "${CURR_DIR}/${RESULT_DIR}"
 echo -e "${PML_BEGIN}\n\n" > "pml_begin.txt"
@@ -62,5 +64,20 @@ PML_CHI_GROUND_IM=2
 PML_M=3
 RESULT_DIR="test_begin=${PML_BEGIN}_width=${PML_WIDTH}_chi-air=(${PML_CHI_AIR_RE},${PML_CHI_AIR_IM})_chi-water=(${PML_CHI_WATER_RE},${PML_CHI_WATER_IM})_chi-ground=(${PML_CHI_GROUND_RE},${PML_CHI_GROUND_IM})_m=${PML_M}"
 calc
+
+for PML_CHI_AIR_RE in `LANG=C seq 3 6` ; do
+for PML_CHI_AIR_IM in `LANG=C seq 0 1` ; do
+for PML_CHI_WATER_RE in `LANG=C seq 0 1` ; do
+for PML_CHI_WATER_IM in `LANG=C seq 5 7` ; do
+for PML_CHI_GROUND_RE in `LANG=C seq 1 3` ; do
+for PML_CHI_GROUND_IM in `LANG=C seq 1 3` ; do
+RESULT_DIR="test_begin=${PML_BEGIN}_width=${PML_WIDTH}_chi-air=(${PML_CHI_AIR_RE},${PML_CHI_AIR_IM})_chi-water=(${PML_CHI_WATER_RE},${PML_CHI_WATER_IM})_chi-ground=(${PML_CHI_GROUND_RE},${PML_CHI_GROUND_IM})_m=${PML_M}"
+calc
+done
+done
+done
+done
+done
+done
 
 exit 0
