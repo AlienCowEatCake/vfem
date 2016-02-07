df4b9357cf16dd7f2815d4a1ecf8997a5f3cd40c
Peter Zhigalov <peter.zhigalov@gmail.com>
2016-02-08 01:29:01 +0600

    Обновил двухслойную задачу с включением и PML

--- data/area_2layers_inc_loop/run.sh
+++ data/area_2layers_inc_loop/run.sh
@@ -10,6 +10,7 @@ cd "${RESULT_DIR}"
 ln -s ../mesh2_inc_z=5_small.msh ./mesh2_inc_z=5_small.msh
 ln -s ../config_pml.ini ./config_pml.ini
 ln -s ../phys_pml.ini ./phys_pml.ini
+ln -s ../area_2layers_inc_loop_slae.txt ./area_2layers_inc_loop_slae.txt
 echo -e "[PML]\nx0 = -600\nx1 = 600\ny0 = -600\ny1 = 600\nz0 = -600\nz1 = 600\nm = 3\nwidth = 100\n" > pml.ini
 echo -e "[PML.21]\nchi_real = ${PML_CHI_AIR_RE}\nchi_imag = ${PML_CHI_AIR_IM}\n" >> pml.ini
 echo -e "[PML.22]\nchi_real = ${PML_CHI_GROUND_RE}\nchi_imag = ${PML_CHI_GROUND_IM}\n" >> pml.ini
