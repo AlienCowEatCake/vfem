f45f9a5272d381fef59d415b473bc0241b2e7923
Petr Zhigalov <p.zhigalov@movavi.com>
2016-10-10 11:16:40 +0700

    Починил сборку под OS X с MacOSX10.6.sdk

--- src/core/utils/cxxversion.h
+++ src/core/utils/cxxversion.h
@@ -8,7 +8,10 @@
     (defined(__GNUC__) && defined(__GNUC_MINOR__) && defined(__GXX_EXPERIMENTAL_CXX0X__) && \
     (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8))) || \
     (defined(_MSC_VER) && _MSC_VER >= 1800)
+#if !defined(__APPLE__) || (defined(__APPLE__) && defined(MAC_OS_X_VERSION_MAX_ALLOWED) && \
+    (MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_6))
 #define USE_CXX11
+#endif
 #endif
 
 #endif // UTILS_CXXVERSION_H_INCLUDED
